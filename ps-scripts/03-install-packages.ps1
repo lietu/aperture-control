4db09a174237db2c073e2967e4f60c85e7283f14
Janne Enberg <janne.enberg@lietu.net>
2019-11-10 15:21:03 +0200

    Added a few packages

--- ps-scripts/03-install-packages.ps1
+++ ps-scripts/03-install-packages.ps1
@@ -1,24 +1,79 @@
 # Install all the packages you want to use in other scripts
 
-# These are just examples, you may want to customize this pretty heavily
-
 choco.exe install -y `
+    7zip.install `
+    adobereader `
     aria2 `
+    audacity `
+    azure-cli `
+    ccleaner `
     chocolateygui `
     conemu `
+    cpu-z.install `
     cyg-get `
     cygwin `
+    dashlane `
+    discord.install `
+    docker-cli `
+    dotnet4.6.1 `
+    everything `
+    foobar2000 `
+    fork `
+    gimp `
     git.install `
+    gitkraken `
+    goggalaxy `
+    golang `
+    gpu-z `
+    helm `
+    hg `
+    irfanview `
+    jetbrainstoolbox `
+    jq `
+    jre8 `
+    kitty `
+    kubernetes-cli `
+    make-mkv `
+    mingw `
+    minikube `
+    mpc-hc `
+    nodejs.install `
+    obs-studio `
+    processexp `
+    pycharm `
+    python2 `
+    python3 `
+    qbittorrent `
+    screentogif `
+    sharex `
     shutup10 `
+    slack `
+    spotify `
+    steam `
+    sublimetext3 `
     sysinternals `
+    telegram.install `
+    tortoisesvn `
+    treesizefree `
     vcbuildtools `
     vcredist-all `
+    veracrypt `
     virtualbox `
+    vivaldi `
+    vscode `
+    whatsapp `
+    winmerge `
+    winscp `
+    yarn `
     wsl `
     # This non-empty line is important, do not move it
 
 scoop.cmd bucket add extras
 
 scoop.cmd install `
+    brave `
     dos2unix `
+    lame `
+    pypy3 `
+    youtube-dl `
     # This non-empty line is important, do not move it
