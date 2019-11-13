bd7be11cb0ae642f605e717bb1046df76d6b0534
Janne Enberg <janne.enberg@lietu.net>
2019-11-13 23:04:59 +0200

    Updated hyper-v recipe

--- recipes/006-hyper-v.ps1
+++ recipes/006-hyper-v.ps1
@@ -1,2 +1,2 @@
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart
 net localgroup "Hyper-V Administrators" $env:USERNAME /add
