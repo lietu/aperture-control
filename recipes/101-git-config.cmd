730f7ad9028b183885a170afae75b5bd8e60c11c
Janne Enberg <janne.enberg@lietu.net>
2019-12-11 19:32:45 +0200

    What fucking idiot keeps adding these fucking antifeatures to git?

--- recipes/101-git-config.cmd
+++ recipes/101-git-config.cmd
@@ -1,3 +1,4 @@
 git config --global user.name "Janne Enberg"
 git config --global user.email "janne.enberg@lietu.net"
 git config --global core.autocrlf false
+git config --global credential.helper ""
