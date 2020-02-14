42b6f60dcdf0021a82954844694f3586f8f79d71
Janne Enberg <janne.enberg@lietu.net>
2020-02-14 23:01:31 +0200

    No more pyenv

--- recipes/304-setup-pyenv.ps1
+++
@@ -1,8 +0,0 @@
-$pyenvPath = "$env:USERPROFILE/.pyenv"
-$reg = "Registry::HKCU\Environment"
-Set-ItemProperty -Path "$reg" -Name PYENV -Value "$pyenvPath"
-$env:PYENV = [System.Environment]::GetEnvironmentVariable("PYENV", "User")
-
-pip install pyenv-win --target $pyenvPath
-pyenv --version
-pyenv rehash
