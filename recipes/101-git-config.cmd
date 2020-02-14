6ddf9826610e080e68ea81f936f0a104f415f3f7
Janne Enberg <janne.enberg@lietu.net>
2020-02-14 23:01:41 +0200

    Git winmerge

--- recipes/101-git-config.cmd
+++ recipes/101-git-config.cmd
@@ -1,4 +1,12 @@
 git config --global user.name "Janne Enberg"
 git config --global user.email "janne.enberg@lietu.net"
 git config --global core.autocrlf false
+
 git config --global credential.helper ""
+
+git config --global diff.tool winmerge
+git config --replace --global difftool.winmerge.cmd "\"C:\Program Files\WinMerge\WinMergeU.exe\" -e \"$LOCAL\" \"$REMOTE\""
+
+git config --global merge.tool winmerge
+git config --replace --global mergetool.winmerge.cmd "\"C:\Program Files\WinMerge\WinMergeU.exe\" -e -u -dl \"Base\" -dr \"Mine\" \"$LOCAL\" \"$REMOTE\" \"$MERGED\""
+git config --global mergetool.prompt false
