5c08e0cfdfc481370777a19280ea306136140b63
Janne Enberg <janne.enberg@lietu.net>
2020-06-16 11:35:25 +0300

    Using git credential helper again because Git is a fucking piece of shit

--- recipes/101-git-config.cmd
+++ recipes/101-git-config.cmd
@@ -2,7 +2,7 @@ git config --global user.name "Janne Enberg"
 git config --global user.email "janne.enberg@lietu.net"
 git config --global core.autocrlf false
 
-git config --global credential.helper ""
+git config --global credential.helper "manager"
 
 git config --global diff.tool winmerge
 git config --replace --global difftool.winmerge.cmd "\"C:\Program Files\WinMerge\WinMergeU.exe\" -e \"$LOCAL\" \"$REMOTE\""
